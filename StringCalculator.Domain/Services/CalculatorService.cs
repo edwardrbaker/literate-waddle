bd3b506aac1869a6312a7b65197b317b9e96a31e
Ed Baker <edward.baker@intlfcstone.com>
2020-02-04 20:39:23 -0600

    add method, when input is 1 number, return that number: green

--- StringCalculator.Domain/Services/CalculatorService.cs
+++ StringCalculator.Domain/Services/CalculatorService.cs
@@ -4,9 +4,18 @@ namespace StringCalculator.Domain.Services
 {
     public class CalculatorService : ICalculatorService
     {
+        // per kata, we should assume the input is valid and not test
+        // otherwise I'd have added some additional bounds testing on `input`
         public int Add(string input)
         {
             if (string.IsNullOrEmpty(input)) { return 0; }
+
+            var numbers = input.Split(',');
+            if (numbers.Length == 1)
+            {
+                int.TryParse(numbers[0], out int result);
+                return result;
+            }
 
             throw new ArithmeticException("wee");
         }
